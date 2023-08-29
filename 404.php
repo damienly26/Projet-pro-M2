0d812b9de69f394616df05020bca7c274df62953
Damien Lamey <71431769+damienly26@users.noreply.github.com>
2023-08-29 14:16:15 +0200

    Files updates

--- 404.php
+++ 404.php
@@ -1,18 +1,18 @@
-<?php get_header(); ?>
-
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-      <section class="error-404 not-found">
-
-
-        <div class="page-content">
-          <?php pll_e("We are sorry but the page you are looking for is not or no longer available."); ?>
-
-        </div>
-      </section>
-
-    </main>
-  </div>
-
-<?php get_footer(); ?>
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+      <section class="error-404 not-found">
+
+
+        <div class="page-content">
+          <?php pll_e("We are sorry but the page you are looking for is not or no longer available."); ?>
+
+        </div>
+      </section>
+
+    </main>
+  </div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-
-  $parent_style = 'parent-style';
-
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style-editor-block.css' );
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style-rtl.css' );
-  wp_enqueue_style( 'child-style',
-    get_stylesheet_directory_uri() . '/style.css',
-    array( $parent_style ),
-    wp_get_theme()->get('Version')
-  );
-}
-
-add_action('init', function() {
-  pll_register_string("page-404", "We are sorry but the page you are looking for is not or no longer available.");
-});
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+
+  $parent_style = 'parent-style';
+
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style-editor-block.css' );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style-rtl.css' );
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( $parent_style ),
+    wp_get_theme()->get('Version')
+  );
+}
+
+add_action('init', function() {
+  pll_register_string("page-404", "We are sorry but the page you are looking for is not or no longer available.");
+});
